61c46f580c55413acf7e3a74c020ca1ef9654431
yangyuekai <yangyuekai03031>
2026-02-06 09:35:03 +0800

    update

--- frontend/bi-dashboard/src/components/NodeConfig/DataNodeConfig.tsx
+++ frontend/bi-dashboard/src/components/NodeConfig/DataNodeConfig.tsx
@@ -78,7 +78,7 @@ const SearchableDropdown: React.FC<{
           className="dropdown-input"
         />
         <span className="dropdown-arrow" onClick={() => setIsOpen(!isOpen)}>
-          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <svg viewBox="0  0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6z" fill="currentColor"/>
           </svg>
         </span>
